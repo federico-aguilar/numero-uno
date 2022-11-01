49a530871805f3d704f56529c18a755a968897cd
jfar41 <68749301+jfar41@users.noreply.github.com>
2022-11-01 14:21:54 -0700

    Separated service from DB retrieval by adding repositories

--- src/Supermarket.API/Services/CategoryService.cs
+++ src/Supermarket.API/Services/CategoryService.cs
@@ -3,13 +3,23 @@ using System.Threading.Tasks;
 using Supermarket.API.Domain.Models;
 using Supermarket.API.Domain.Services;
 
+// A service class is not a class that should handle data access: 
+// Repository Pattern exists to manage data from DB
+
 namespace Supermaket.API.Services
 {
     public class CategoryService : ICategoryService
     {
+        private readonly ICategoryRepository _categoryRepository;
+
+        public CategoryService(ICategoryRepository categoryRepository)
+        {
+            this._categoryRepository = categoryRepository;
+        }
+
         public async Task<IEnumerable<Category>> ListAsync()
         {
-            
+            return await _categoryRepository.ListAsync();
         }
     }
 }
